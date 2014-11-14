97c4b3a7ede91a8fd188b771594c1cf773ceb86f
Brian <bpruitt-goddard@github>
2014-11-13 23:03:53 -0500

    Fix failing session test
    Session create test failing due to lack of data
    Added proper data for test to pass
    Created test for show

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,13 +1,27 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
+  test 'should post create' do
+    @session_players = [
+        {
+            'player_id' => 1,
+            'placing' => 1,
+            'score' => 1
+        }
+    ]
+
+    post :create, {'game_id' => 1, 'session_players' => @session_players}
     assert_response :success
   end
 
-  test "should get show" do
-    get :show
+  # TODO Add this back when show is available
+  # test 'should get show' do
+  #   get(:show, {'id' => '1'})
+  #   assert_response :success
+  # end
+
+  test 'should get index' do
+    get :index
     assert_response :success
   end
 
