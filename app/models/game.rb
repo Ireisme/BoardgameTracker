7c5efdc7c21b91934ad640f2d59b34751ddbfb55
Brian <bpruitt-goddard@github>
2014-11-17 00:16:13 -0500

    Refactor game stats
    Move game win calculation from controller to model
    Add tests to verify behavior

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,27 @@
 class Game < ActiveRecord::Base
   has_many :sessions
+
+  def best_player
+    game_totals.first
+  end
+
+  def worst_player
+    game_totals.last
+  end
+
+  private
+  def game_totals
+    # Have to declare win_percentage because (wins / plays) doesn't work in ordering
+    # In win_percentage calculation, have to cast one operand to float to ensure float result
+    Session.joins(:session_player)
+      .select('session_players.player_id,
+               count(*) as plays,
+               sum(case "placing"
+                  when 1 then 1
+                  else 0 end) as wins,
+               (sum(CASE "placing" WHEN 1 THEN 1 ELSE 0 END) / count(*)::float) as win_percentage')
+      .where('game_id = ?', id)
+      .group('session_players.player_id')
+      .order('win_percentage desc')
+  end
 end
