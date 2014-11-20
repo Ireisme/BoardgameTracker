4de13b2f2d0c168eb2d6d88987d51a1b6486ea36
Brian <bpruitt-goddard@github>
2014-11-19 23:46:06 -0500

    Pluralization
    Properly pluralize has_many relations

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,6 @@
 class Session < ActiveRecord::Base
   belongs_to :game
   
-  has_many :session_player
-  has_many :player,  through: :session_player
+  has_many :session_players
+  has_many :players,  through: :session_player
 end
