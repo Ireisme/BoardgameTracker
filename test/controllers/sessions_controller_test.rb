bab296a9e40044ec304780593ab9a63bfed3c3e8
Brian <bpruitt-goddard@github>
2015-06-26 02:53:30 -0400

    Prevent duplicate player in session
    Allow unknown player to be added multiple times,
    but not any unique player.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -14,6 +14,48 @@ class Game::SessionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'should not be able to add player twice to session' do
+    @session_players = [
+        {
+            'player_id' => players(:josh).id,
+            'placing' => 1,
+            'score' => 2
+        },
+        {
+            'player_id' => players(:josh).id,
+            'placing' => 2,
+            'score' => 1
+        }
+    ]
+
+    assert_raises(ActionController::BadRequest) do
+      post :create, {'game_id' => 1, 'session_players' => @session_players}
+    end
+  end
+
+  test 'should be able to add unknown player multiple times to session' do
+    @session_players = [
+        {
+            'player_id' => players(:brian).id,
+            'placing' => 2,
+            'score' => 1
+        },
+        {
+            'player_id' => players(:unknown).id,
+            'placing' => 1,
+            'score' => 2
+        },
+        {
+            'player_id' => players(:unknown).id,
+            'placing' => 2,
+            'score' => 1
+        }
+    ]
+
+    post :create, {'game_id' => 1, 'session_players' => @session_players}
+    assert_response :success
+  end
+
   # TODO Add this back when show is available
   # test 'should get show' do
   #   get(:show, {'id' => '1'})
