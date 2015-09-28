011540d312eebaeb8787df5505772a3d932edba5
Brian <bpruitt-goddard@github>
2015-09-28 00:53:18 -0400

    Add default ordering to game controller
    Fix typo in session relationship

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,5 +2,5 @@ class Session < ActiveRecord::Base
   belongs_to :game
   
   has_many :session_players
-  has_many :players,  through: :session_player
+  has_many :players, through: :session_players
 end
