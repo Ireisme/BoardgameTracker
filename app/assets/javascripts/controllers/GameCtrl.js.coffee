f2da2f396cb231393ed62bc3a382643118c6138f
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-06-04 21:14:35 -0700

    fix(games): Add best/worst player stats to serialized response and show

--- app/assets/javascripts/controllers/GameCtrl.js.coffee
+++ app/assets/javascripts/controllers/GameCtrl.js.coffee
@@ -3,5 +3,7 @@ angular.module('BoardgameTracker')
   GamesService.Get($stateParams.id)
     .then (data) -> 
       $scope.game = data
+      $scope.game.best_player.percentage = Math.round $scope.game.best_player.percent * 100
+      $scope.game.worst_player.percentage = Math.round $scope.game.worst_player.percent * 100
       return
   ]
--- app/serializers/games/show_serializer.rb
+++ app/serializers/games/show_serializer.rb
@@ -1,3 +1,11 @@
 class Games::ShowSerializer < GameSerializer
   attributes :best_player, :worst_player
+
+  def best_player
+    {:percent => object.best_player.win_percent, :name => object.best_player.player.name }
+  end
+
+  def worst_player
+    {:percent => object.worst_player.win_percent, :name => object.worst_player.player.name }
+  end
 end
