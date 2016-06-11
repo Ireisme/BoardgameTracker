1d0b1053482f6b1bcfa99b25ed8589cf9703dbac
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-06-10 22:06:58 -0700

    feat(gametype): Add game_type attribute to Game model
    Defaults to value of competitive
    Create associated enum on Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,10 @@
 class Game < ActiveRecord::Base
   has_many :sessions
+  enum game_type: {
+      mixed: 0,
+      competitive: 1,
+      coop: 2
+  }
 
   default_scope { order('name asc') }
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018033613) do
+ActiveRecord::Schema.define(version: 20160610052711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151018033613) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+    t.integer  "game_type",   null: false
   end
 
   create_table "players", force: true do |t|
