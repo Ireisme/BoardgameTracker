8571e8d86830096fcdf8bdbdc49681b5d968ecca
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-06-16 23:34:39 -0700

    fix(worst_player): Return a single worst player when tied for worst

--- test/models/concerns/games/stats_retriever_test.rb
+++ test/models/concerns/games/stats_retriever_test.rb
@@ -12,6 +12,17 @@ class StatsRetrieverTest < ActionController::TestCase
     assert_includes [winning_player1, winning_player2], game.best_player.player
   end
 
+  test 'should return a player as worst player when tied for worst' do
+    game = create_game
+    winning_player = create_player('winner')
+    losing_player1 = create_player('loser1')
+    losing_player2 = create_player('loser2')
+
+    create_session(game, winning_player, losing_player1, losing_player2)
+
+    assert_includes [losing_player1, losing_player2], game.worst_player.player
+  end
+
   test 'should return null as worst player when all players are tied for stats' do
     game = create_game
     player1 = create_player
