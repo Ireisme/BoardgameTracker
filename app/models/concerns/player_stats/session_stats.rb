4519cf28f4f0dafc761c6462a3c2ba0c6767b745
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-06-26 01:37:07 -0700

    feat(favorite_game): Add calculation for a player's favorite game

--- app/models/concerns/player_stats/session_stats.rb
+++ app/models/concerns/player_stats/session_stats.rb
@@ -5,5 +5,21 @@ module PlayerStats
       def session_count
         SessionPlayer.where('player_id = ?', @player_id).distinct.count(:session_id)
       end
+
+      def favorite_game
+        subquery = SessionPlayer.joins(:session)
+                       .where('player_id = ?', @player_id)
+                       .group('sessions.id')
+                       .select('sessions.game_id')
+
+        game_id = Game.from(subquery)
+                      .group('game_id')
+                      .select('game_id')
+                      .order('count(game_id) desc')
+                      .first
+                      .game_id
+
+        return Game.find(game_id)
+      end
   end
 end
--- test/models/concerns/player_stats/session_stats_test.rb
+++ test/models/concerns/player_stats/session_stats_test.rb
@@ -20,4 +20,28 @@ class SessionStatsTest < ActionController::TestCase
 
     assert_equal 1, session_count
   end
+
+  test 'should calculate favorite game based upon most sessions played' do
+    player = create_player
+    game_one = create_game('favorite')
+    game_two = create_game
+    2.times { create_session(game_one, winning_players: [player]) }
+    create_session(game_two, winning_players: [player])
+
+    favorite_game = PlayerStatistic.new(player.id).favorite_game
+
+    assert_equal game_one, favorite_game
+  end
+
+  test 'should count sessions with duplicate players once when calculating favorite game' do
+    player = create_player
+    game_one = create_game('favorite')
+    game_two = create_game
+    3.times { create_session(game_one, winning_players:[player]) }
+    2.times { create_session(game_two, winning_players:[player, player]) }
+
+    favorite_game = PlayerStatistic.new(player.id).favorite_game
+
+    assert_equal game_one, favorite_game
+  end
 end
