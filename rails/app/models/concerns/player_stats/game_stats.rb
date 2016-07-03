61f2ff01f7992f3903287c448a4269dea10ae46c
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-07-03 15:51:58 -0700

    feat(worst_game) [server]: Add calculation for a player's worst game

--- rails/app/models/concerns/player_stats/game_stats.rb
+++ rails/app/models/concerns/player_stats/game_stats.rb
@@ -20,6 +20,24 @@ module PlayerStats
         return to_percent(query_result)
       end
 
+      def worst_game
+        subquery = SessionPlayer.joins(:session)
+                       .group('sessions.id, sessions.game_id')
+                       .where('player_id = ?', @player_id)
+                       .select('sessions.game_id,
+                          CASE MAX("placing") WHEN 1 THEN 1 ELSE 0 END as didWin ')
+
+        # In win_percentage calculation, have to cast one operand to float to ensure float result
+        query_result = Game.from(subquery)
+                           .group('game_id')
+                           .select('game_id,
+                         sum(subquery.didWin) / count(*)::float as win_percentage')
+                           .order('win_percentage asc')
+                           .first
+
+        return to_percent(query_result)
+      end
+
       private
         def to_percent (game_total)
           return nil unless game_total
--- rails/test/models/concerns/player_stats/game_stats_test.rb
+++ rails/test/models/concerns/player_stats/game_stats_test.rb
@@ -3,59 +3,119 @@ require 'test_helper'
 class GameStatsTest < ActiveSupport::TestCase
   include GameCreation, PlayerCreation, SessionCreation
 
-  should 'calculate best game based upon win %' do
-    player = create_player
-    other_player = create_player
-    game_one = create_game ('best')
-    game_two = create_game
-    3.times { create_session(game_one, winning_players: [player]) }
-    create_session(game_two, winning_players: [player], losing_players: [other_player])
-    create_session(game_two, winning_players: [other_player], losing_players: [player])
+  context 'best game calculation' do
+    should 'should calculate based upon win %' do
+      player = create_player
+      other_player = create_player ('other')
+      game_one = create_game ('best')
+      game_two = create_game
+      3.times { create_session(game_one, winning_players: [player]) }
+      create_session(game_two, winning_players: [player], losing_players: [other_player])
+      create_session(game_two, winning_players: [other_player], losing_players: [player])
 
-    best_game = PlayerStatistic.new(player.id).best_game
+      best_game = PlayerStatistic.new(player.id).best_game
 
-    assert_equal game_one, best_game.game
-    assert_equal 1, best_game.win_percent
+      assert_equal game_one, best_game.game
+      assert_equal 1, best_game.win_percent
+    end
+
+    should 'should calculate win % correctly for normal case' do
+      player = create_player
+      game = create_game ('best')
+      3.times { create_session(game, winning_players: [player]) }
+      create_session(game, losing_players: [player])
+
+      best_game = PlayerStatistic.new(player.id).best_game
+
+      assert_equal game, best_game.game
+      assert_equal 0.75, best_game.win_percent
+    end
+
+    should 'should chose an arbitrary game when multiple are tied' do
+      player = create_player
+      game_one = create_game
+      game_two = create_game ('other')
+      create_session(game_one, winning_players: [player])
+      create_session(game_two, winning_players: [player])
+
+      best_game = PlayerStatistic.new(player.id).best_game
+
+      assert_includes [game_one, game_two], best_game.game
+      assert_equal 1, best_game.win_percent
+    end
+
+    should 'should choose best placing in game when playing as multiple players' do
+      player = create_player
+      game_one = create_game
+      game_two = create_game ('other')
+      create_session(game_one, winning_players: [player])
+      create_session(game_one, losing_players: [player])
+      create_session(game_two, winning_players: [player])
+      create_session(game_two, losing_players: [player])
+      2.times { create_session(game_one, winning_players: [player], losing_players: [player]) }
+
+      best_game = PlayerStatistic.new(player.id).best_game
+
+      assert_equal game_one, best_game.game
+      assert_equal 0.75, best_game.win_percent
+    end
   end
 
-  should 'calculate win % correctly for normal case' do
-    player = create_player
-    game = create_game ('best')
-    3.times { create_session(game, winning_players: [player]) }
-    create_session(game, losing_players: [player])
+  context 'worst game calculation' do
+    should 'should calculate based upon loss %' do
+      player = create_player
+      other_player = create_player ('other')
+      game_one = create_game ('worst')
+      game_two = create_game
+      3.times { create_session(game_one, losing_players: [player]) }
+      create_session(game_two, winning_players: [other_player], losing_players: [player])
+      create_session(game_two, winning_players: [player], losing_players: [other_player])
 
-    best_game = PlayerStatistic.new(player.id).best_game
+      worst_game = PlayerStatistic.new(player.id).worst_game
 
-    assert_equal game, best_game.game
-    assert_equal 0.75, best_game.win_percent
-  end
+      assert_equal game_one, worst_game.game
+      assert_equal 0, worst_game.win_percent
+    end
 
-  should 'chose an arbitrary game when multiple are tied' do
-    player = create_player
-    game_one = create_game
-    game_two = create_game ('other')
-    create_session(game_one, winning_players: [player])
-    create_session(game_two, winning_players: [player])
+    should 'should calculate win % correctly for normal case' do
+      player = create_player
+      game = create_game ('worst')
+      3.times { create_session(game, losing_players: [player]) }
+      create_session(game, winning_players: [player])
 
-    best_game = PlayerStatistic.new(player.id).best_game
+      worst_game = PlayerStatistic.new(player.id).worst_game
 
-    assert_includes [game_one, game_two], best_game.game
-    assert_equal 1, best_game.win_percent
-  end
+      assert_equal game, worst_game.game
+      assert_equal 0.25, worst_game.win_percent
+    end
 
-  should 'choose best placing in game when playing as multiple players' do
-    player = create_player
-    game_one = create_game
-    game_two = create_game ('other')
-    create_session(game_one, winning_players: [player])
-    create_session(game_one, losing_players: [player])
-    create_session(game_two, winning_players: [player])
-    create_session(game_two, losing_players: [player])
-    2.times { create_session(game_one, winning_players: [player], losing_players: [player]) }
+    should 'should chose an arbitrary game when multiple are tied' do
+      player = create_player
+      game_one = create_game
+      game_two = create_game ('other')
+      create_session(game_one, losing_players: [player])
+      create_session(game_two, losing_players: [player])
 
-    best_game = PlayerStatistic.new(player.id).best_game
+      worst_game = PlayerStatistic.new(player.id).worst_game
 
-    assert_equal game_one, best_game.game
-    assert_equal 0.75, best_game.win_percent
+      assert_includes [game_one, game_two], worst_game.game
+      assert_equal 0, worst_game.win_percent
+    end
+
+    should 'should choose worst placing in game when playing as multiple players' do
+      player = create_player
+      game_one = create_game
+      game_two = create_game ('other')
+      create_session(game_one, winning_players: [player])
+      create_session(game_one, losing_players: [player])
+      create_session(game_two, winning_players: [player])
+      create_session(game_two, losing_players: [player])
+      2.times { create_session(game_one, winning_players: [player], losing_players: [player]) }
+
+      best_game = PlayerStatistic.new(player.id).worst_game
+
+      assert_equal game_one, best_game.game
+      assert_equal 0.25, best_game.win_percent
+    end
   end
 end
