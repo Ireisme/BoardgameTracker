1668fab160d24f0a4c4c87d3c876deda428b779a
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-07-21 23:03:55 -0700

    feat(current_streak)[server]: Add calculation for players current streak

--- rails/app/models/concerns/player_stats/game_streak_stats.rb
+++ rails/app/models/concerns/player_stats/game_streak_stats.rb
@@ -58,5 +58,42 @@ module PlayerStats
         results = ActiveRecord::Base.connection.execute(sql)
         return results.to_a.first['longest_streak'].to_i
       end
+
+      def current_streak
+        sql = "
+        SELECT
+        S1.ID,
+        T1.\"placing\",
+        (
+            SELECT 1 + COUNT(*)                 -- Earlier games with the same player
+            FROM sessions S2
+            inner join session_players T2 on S2.id = T2.session_id
+            where ((T1.\"placing\" > 1 and T2.\"placing\" > 1) or (T1.\"placing\" = 1 and T2.\"placing\" = 1))
+            and T1.player_id = T2.player_id
+            AND (S1.played > S2.played OR (S1.played = S2.played AND S1.created_at > S2.created_at))
+            AND NOT EXISTS (
+              SELECT *                    -- The games in between, with the same player but opposite result.
+              FROM sessions S3
+              inner join session_players T3 on S3.id = T3.session_id
+              WHERE
+                  ((T2.\"placing\" > 1 and T3.\"placing\" = 1) or (T2.\"placing\" = 1 and T3.\"placing\" > 1))
+                  AND T1.player_id = T3.player_id
+                  AND (S3.played > S2.played OR (S2.played = S3.played AND S3.created_at > S2.created_at))
+	                AND (S3.played < S1.played OR (S1.played = S3.played AND S3.created_at < S1.created_at))
+            )
+        ) streak
+        FROM sessions S1
+        --TODO get this joining
+        inner join session_players T1 on T1.session_id = S1.id
+        where T1.player_id = #{@player_id}
+        order by S1.played desc, S1.created_at desc
+        limit 1;"
+        results = ActiveRecord::Base.connection.execute(sql)
+        return 0 if results.num_tuples.zero?
+
+        is_win_streak = results.to_a.first['placing'].to_i == 1
+        streak_count = results.to_a.first['streak'].to_i
+        return is_win_streak ? streak_count : -streak_count
+      end
   end
 end
--- rails/test/models/concerns/player_stats/game_streak_stats_test.rb
+++ rails/test/models/concerns/player_stats/game_streak_stats_test.rb
@@ -87,4 +87,79 @@ class GameStreakStatsTest < ActiveSupport::TestCase
       assert_equal 2, best_streak
     end
   end
+
+  context 'current streak' do
+    should 'return 0 when no games have yet been played by player' do
+      player = create_player
+
+      current_streak = PlayerStatistic.new(player.id).current_streak
+
+      assert_equal 0, current_streak
+    end
+
+    should 'return a positive value when the streak is 1 win' do
+      player = create_player
+      game = create_game
+      create_session(game, winning_players: [player])
+
+      current_streak = PlayerStatistic.new(player.id).current_streak
+
+      assert_equal 1, current_streak
+    end
+
+    should 'return a positive value when the streak is multiple wins' do
+      player = create_player
+      game = create_game
+      2.times { create_session(game, winning_players: [player]) }
+
+      current_streak = PlayerStatistic.new(player.id).current_streak
+
+      assert_equal 2, current_streak
+    end
+
+    should 'return a negative value when the streak is 1 loss' do
+      player = create_player
+      game = create_game
+      create_session(game, losing_players: [player])
+
+      current_streak = PlayerStatistic.new(player.id).current_streak
+
+      assert_equal -1, current_streak
+    end
+
+    should 'return a negative value when the streak is multiple wins' do
+      player = create_player
+      game = create_game
+      2.times { create_session(game, losing_players: [player]) }
+
+      current_streak = PlayerStatistic.new(player.id).current_streak
+
+      assert_equal -2, current_streak
+    end
+
+    should 'reset streak counter after a different result' do
+      player = create_player
+      game = create_game
+      start_date = Date.today
+      create_session(game, losing_players: [player], played: start_date)
+      create_session(game, winning_players: [player], played: (start_date + 1))
+
+      current_streak = PlayerStatistic.new(player.id).current_streak
+
+      assert_equal 1, current_streak
+    end
+
+    should 'order games created on same date by create ordering when determining streak' do
+      player = create_player
+      game = create_game
+      start_date = Date.today
+      create_session(game, winning_players: [player], played: start_date)
+      create_session(game, losing_players: [player], played: start_date)
+      2.times { create_session(game, winning_players: [player], played: start_date) }
+
+      current_streak = PlayerStatistic.new(player.id).current_streak
+
+      assert_equal 2, current_streak
+    end
+  end
 end
