15c97d163598bced304cce1a104f6ab024b05abc
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-10-25 17:10:40 -0700

    refactor(rails)[rails]: Convert rails to rails-api

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
   respond_to :html, :json
 
   def not_found
