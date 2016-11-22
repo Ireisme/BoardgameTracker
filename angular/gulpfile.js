7cf43567b641ee95688c1d4d946f982063bc5bff
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-11-21 19:24:14 -0800

    refactor(gulp)[angular]: Move client build tasks to subdir

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,31 +1,12 @@
+var fs = require('fs');
 var gulp = require('gulp');
-var webpack = require('webpack-stream');
-var templateCache = require('gulp-angular-templatecache');
-var browserSync = require('browser-sync');
 
-gulp.task('views', function() {
-  gulp.src(['index.html'])
-    .pipe(gulp.dest('build/'))
+/**
+ *  This will load all js or coffee files in the gulp directory
+ *  in order to load all gulp tasks
+ */
+fs.readdirSync('./gulp').filter(function(file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function(file) {
+  require('./gulp/' + file);
 });
-
-gulp.task('webpack', ['views'], function() {
-  return gulp.src('app/app.js.coffee')
-    .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('build/js/'));
-});
-
-gulp.task('styles', function() {
-  return gulp.src('css/**/*.css')
-    .pipe(gulp.dest('build/css/'));
-});
-
-gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: './build'
-    },
-    port: '8888'
-  })
-});
-
-gulp.task('dev', ['views', 'webpack', 'styles', 'serve']);
