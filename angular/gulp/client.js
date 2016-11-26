6b25aaff80079b0f8b3df5cb14e888fa59f40909
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-11-25 23:32:35 -0800

    refactor(gulp)[angular]: Clean up browsersync build step

--- angular/gulp/client.js
+++ angular/gulp/client.js
@@ -3,6 +3,20 @@ var webpack = require('webpack-stream');
 var templateCache = require('gulp-angular-templatecache');
 var browserSync = require('browser-sync');
 
+function browserSyncInit(baseDir, browser) {
+  browser = browser === undefined ? 'default' : browser;
+
+  var server = {
+    baseDir: baseDir
+  };
+
+  browserSync.instance = browserSync.init({
+    server: server,
+    browser: browser,
+    port: '8888'
+  });
+}
+
 gulp.task('views', function() {
   gulp.src(['index.html'])
     .pipe(gulp.dest('build/'))
@@ -28,12 +42,7 @@ return gulp.src('css/**/*.css')
 });
 
 gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: './build'
-    },
-    port: '8888'
-  })
+  browserSyncInit('./build', []);
 });
 
 gulp.task('client', ['views', 'webpack', 'styles', 'serve']);
