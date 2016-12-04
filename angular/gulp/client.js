dab480f74847ece818ba99eee2f0d2521bd98261
Brian Pruitt-Goddard <bpruitt-goddard@github>
2016-12-04 00:47:04 -0800

    refactor(gulp)[angular]: Move paths and ports to config file

--- angular/gulp/client.js
+++ angular/gulp/client.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var templateCache = require('gulp-angular-templatecache');
 var browserSync = require('browser-sync');
+var path = require('path');
+var conf = require('./conf');
 
 function browserSyncInit(baseDir, browser) {
   browser = browser === undefined ? 'default' : browser;
@@ -13,36 +15,36 @@ function browserSyncInit(baseDir, browser) {
   browserSync.instance = browserSync.init({
     server: server,
     browser: browser,
-    port: '8888'
+    port: conf.ports.client
   });
 }
 
 gulp.task('views', function() {
   gulp.src(['index.html'])
-    .pipe(gulp.dest('build/'))
+  .pipe(gulp.dest(conf.clientPaths.build))
 
-  return gulp.src('app/templates/**/*.html')
+  return gulp.src(path.join(conf.clientPaths.app, 'templates/**/*.html'))
     .pipe(templateCache({
       moduleSystem: 'browserify',
       templateHeader: 'function($templateCache) {\n',
       templateFooter: '\n}\nmodule.exports.$inject = ["$templateCache"];'
     }))
-    .pipe(gulp.dest('app'));
+    .pipe(gulp.dest(conf.clientPaths.app));
 });
 
 gulp.task('webpack', ['views'], function() {
-  return gulp.src('app/app.js.coffee')
+  return gulp.src(path.join(conf.clientPaths.app, 'app.js.coffee'))
     .pipe(webpack(require('../webpack.config.js')))
-    .pipe(gulp.dest('build/js/'));
+    .pipe(gulp.dest(path.join(conf.clientPaths.build, 'js/')));
 });
 
 gulp.task('styles', function() {
 return gulp.src('css/**/*.css')
-    .pipe(gulp.dest('build/css/'));
+    .pipe(gulp.dest(path.join(conf.clientPaths.build, 'css/')));
 });
 
 gulp.task('serve', function() {
-  browserSyncInit('./build', []);
+  browserSyncInit('./' + conf.clientPaths.build, []);
 });
 
 gulp.task('client', ['views', 'webpack', 'styles', 'serve']);
---
+++ angular/gulp/conf.js
@@ -0,0 +1,9 @@
+module.exports = {
+  clientPaths: {
+    build: 'build',
+    app: 'app',
+  },
+  ports: {
+      client: 8888
+  }
+}
